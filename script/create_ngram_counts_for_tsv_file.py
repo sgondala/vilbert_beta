1f752593e5ccb06e4c275ea0a8cd26ee13c44f4f
Sashank Gondala <sgondala2@gmail.com>
2020-02-24 14:46:11 -0500

    Minor modifications

--- script/create_ngram_counts_for_tsv_file.py
+++ script/create_ngram_counts_for_tsv_file.py
@@ -3,26 +3,37 @@ from collections import defaultdict
 import textblob
 import csv
 from textblob import TextBlob
+import cPickle as pickle 
 
-count_dict = defaultdict(lambda: 0)
+# def default_():
+#  return 0
+
+# count_dict = defaultdict(default_)
+count_dict = {}
 
 i = 0
-with open('myfile.tsv') as tsvfile:
+with open('../data/Train_GCC-training.tsv') as tsvfile:
     reader = csv.reader(tsvfile, delimiter='\t')
     for row in reader:
-        if i > 100:
-            break
-        print(i)
-        caption = row[0].lower()
-        unigrams = TextBlob(caption).ngrams(1)
+        if i % 1000 == 0:
+            print(i)
+        caption = row[0].lower().decode('ascii', errors="ignore")
+        try:
+            unigrams = TextBlob(caption).ngrams(1)
+        except:
+            print(caption)
+            assert False
         bigrams = TextBlob(caption).ngrams(2)
         trigrams = TextBlob(caption).ngrams(3)
         fourgrams = TextBlob(caption).ngrams(4)
         allgrams = []
         allgrams += unigrams + bigrams + trigrams + fourgrams
         for value in allgrams:
-            count_dict[tuple(value)] += 1
+            value = tuple(value)
+            if value in count_dict: 
+                count_dict[value] += 1
+            else:
+                count_dict[value] = 1
         i += 1
 
-print(count_dict)
-
+pickle.dump(count_dict, open('conceptual-caps-val-df.p', 'w'))
