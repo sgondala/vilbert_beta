cd031af954166dbf0552b0b1b0265f44e9e7cd6c
RachelZhong98 <ruicanzhong1998@gmail.com>
2020-03-05 20:42:29 -0500

    batch SLOR

--- Scores/SLOR.py
+++ Scores/SLOR.py
@@ -1,21 +1,41 @@
 
+
 import numpy as np
 
-##
-## Unigram_prob: a dictionary that contains all the vocabulary; use coco corpus
-## Log probability of the sentence evaluated using the language model
-## Sentence: predicted caption
-## returns -1 if there are unknown words
-def score(unigram_prob, logprob, sentence):
-    uni = 0.0
-    if sentence.endswith('.'):
-        sentence = sentence[:-1]
-    for w in sentence.split():
-        if not w.lower()[-1].isalpha():
-            w = w.lower()[:-1]
-        if w.lower() not in unigram_prob.keys():
-            return -1
-        uni += np.log(unigram_prob[w.lower()])
-    n = len(sentence.split())
-    return ((logprob - uni) / n)
+from nltk.tokenize import word_tokenize
+from typing import Any, Dict, List, Tuple, Union
+PUNCTUATIONS: List[str] = [
+    "''", "'", "``", "`", "(", ")", "{", "}",
+    ".", "?", "!", ",", ":", "-", "--", "...", ";", '$']
+# def score(unigram_prob_dict, log_prob, captions):
+#     i = 0
+#     batch_score = []
+#     for caption in captions:
+#         caption_tokens: List[str] = word_tokenize(caption)
+#         caption_tokens = [ct.lower() for ct in caption_tokens if ct not in PUNCTUATIONS]
+#         print(caption_tokens)
 
+#         curr_uni = 0.0
+#         for token in caption_tokens:
+#             if token not in unigram_prob_dict.keys():
+#                 return -1 ## change this handling of oov?
+#             curr_uni += np.log(unigram_prob_dict[token])
+#         print(len(caption_tokens))
+#         score = (log_prob[i] - curr_uni) / len(caption_tokens)
+        
+#         batch_score.append(score)
+#         i += 1
+
+#     return batch_score
+def score(unigram_prob_dict, logprob, caption):
+    caption_tokens: List[str] = word_tokenize(caption)
+    caption_tokens = [ct.lower() for ct in caption_tokens if ct not in PUNCTUATIONS]
+
+    curr_uni = 0.0
+    for token in caption_tokens:
+        if token not in unigram_prob_dict.keys():
+            return -1 ## change this handling of oov?
+        curr_uni += np.log(unigram_prob_dict[token])
+    n = len(caption.split())
+
+    return ((logprob - curr_uni) / n)
--- Scores/batch_slor_example.py
+++ Scores/batch_slor_example.py
@@ -5,51 +5,90 @@ import json
 import numpy as np
 import torch
 from loguru import logger
+from SLOR import score
 
 import pandas as pd
 import sys
 
-def cal_log_prob(batch_caption):
-    batch_tokens = [tokenizer.convert_tokens_to_ids(
-        tokenizer.tokenize(x, add_prefix_space=True))
-        for x in batch_caption]
+import json
+from collections import Counter
+captions = []
+with open('coco_annotations_2014/captions_train2014.json') as f:
+    data = json.load(f)
+    for ann in data['annotations']:
+        captions.append(ann['caption'])
+vocab = []
+for cap in captions:
+    if cap.endswith('.'):
+        cap = cap[:-1]
+    for i in cap.split():
+        vocab.append(i.lower())
+vocab_dict = Counter(vocab)
+total = sum(vocab_dict.values())
+unigram_prob = {k: v/total for k, v in vocab_dict.items()}
 
-    # add BOS and EOS
-    tokenize_input = [[tokenizer.bos_token_id] + x + [tokenizer.eos_token_id]
-            for x in batch_tokens]
-    mask = []
-    for sentence in tokenize_input:
-        curr_mask = [1]*len(sentence) + [0] * (20-len(sentence))
-        mask.append(curr_mask)
-    mask = torch.FloatTensor(mask)
-    for i in range(len(tokenize_input)):
-        tokenize_input[i] = tokenize_input[i][: 20]
-        tokenize_input[i].extend([tokenizer.eos_token_id] * (20 - len(tokenize_input[i]))) 
-    tokenize_input = torch.LongTensor(tokenize_input)
+
+def cal_log_prob(caption):
+    # batch_tokens = [tokenizer.convert_tokens_to_ids(
+    #     tokenizer.tokenize(x, add_prefix_space=True))
+    #     for x in batch_caption]
+
+    # # add BOS and EOS
+    # tokenize_input = [[tokenizer.bos_token_id] + x + [tokenizer.eos_token_id]
+    #         for x in batch_tokens]
+    # mask = []
+    # for sentence in tokenize_input:
+    #     curr_mask = [1]*len(sentence) + [0] * (20-len(sentence))
+    #     mask.append(curr_mask)
+    # mask = torch.FloatTensor(mask)
+    # for i in range(len(tokenize_input)):
+    #     tokenize_input[i] = tokenize_input[i][: 20]
+    #     tokenize_input[i].extend([tokenizer.eos_token_id] * (20 - len(tokenize_input[i]))) 
+    # tokenize_input = torch.LongTensor(tokenize_input)
+    # with torch.no_grad():
+    #     outputs = model(tokenize_input, labels=tokenize_input, attention_mask = mask)
+    #     loss, logits = outputs[:2]
+    #     print(logits.size())
+    # batch_lp = []
+    # for index in range(len(tokenize_input)):
+    #     lp = 0.0
+    #     for j in range(len(tokenize_input[index])):
+    #         if mask[index][j]:
+    #             masked_index = j
+    #             predicted_score = logits[index][masked_index]
+    #             predicted_prob = torch.nn.functional.softmax(predicted_score)
+    #             lp += np.log(predicted_prob[tokenizer.convert_tokens_to_ids([tokenize_input[index][j]])[0]])
+    #     batch_lp.append(float(lp))
+    # return batch_lp
+
+    tokenize_input = tokenizer.tokenize(caption)
+        #50256 is the token_id for <|endoftext|>
+    tensor_input = torch.tensor([ [tokenizer.bos_token_id] + tokenizer.convert_tokens_to_ids(tokenize_input) + [tokenizer.eos_token_id]])
     with torch.no_grad():
-        outputs = model(tokenize_input, labels=tokenize_input, attention_mask = mask)
+        outputs = model(tensor_input, labels=tensor_input)
         loss, logits = outputs[:2]
-        
-    batch_lp = []
-    for index in range(len(tokenize_input)):
-        lp = 0.0
-        for j in range(len(tokenize_input[index])):
-            if mask[index][j]:
-                masked_index = j
-                predicted_score = logits[index][masked_index]
-                predicted_prob = torch.nn.functional.softmax(predicted_score)
-                lp += np.log(predicted_prob[tokenizer.convert_tokens_to_ids([tokenize_input[index][j]])[0]])
-        batch_lp.append(lp)
-    return batch_lp
+    lp = 0.0
+    for i in range(len(tokenize_input)):
+        masked_index = i
+        predicted_score = logits[0, masked_index]
+        predicted_prob = torch.nn.functional.softmax(predicted_score)
+        lp += np.log(predicted_prob[tokenizer.convert_tokens_to_ids([tokenize_input[i]])[0]])
+    return float(lp)
+
 
 
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 model = GPT2LMHeadModel.from_pretrained('gpt2')
-captions = ['This is a sentence',
-'A dog is barking at a man',
-'Imminent change is required',
-'A man with a red helmet is riding a bike']
-cal_log_prob(captions)
+captions = [ "a white and white bird standing on a tree",
+            "a bunch of fruits are sitting on a table",
+            "a kitchen with a microwave in a kitchen",
+            "a clock is sitting on top of a clock"]
+scores = []
+for i in captions:
+    log_prob= (cal_log_prob(i))
+    curr_score = score(unigram_prob, log_prob, i)
+    scores.append(curr_score)
+    
 
 logger.add(sys.stdout)
